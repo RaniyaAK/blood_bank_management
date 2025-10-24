89d52a3e92ba0ce97a1788ea8f397ec57a003260
Raniya <raniyaak4@gmail.com>
2025-10-24 09:50:24 -0700

    24

--- blood_bank_app/urls.py
+++ blood_bank_app/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('dashboard/hospital/', views.hospital, name='hospital'),
     path('dashboard/hospital_details_form/', views.hospital_details_form, name='hospital_details_form'),
     path('dashboard/hospital/edit/', views.donor_details_edit, name='donor_edit'),
-    path('dashboard/hospital/info', views.hospital_dashboard, name='hospital_dashboard'),
+    path('dashboard/hospital/info/', views.hospital_dashboard, name='hospital_dashboard'),
 
     path('dashboard/donor/', views.donor, name='donor'),
     path('dashboard/donor_details_form/', views.donor_details_form, name='donor_details_form'),
@@ -25,13 +25,13 @@ urlpatterns = [
     path('dashboard/recipient/', views.recipient, name='recipient'),
     path('dashboard/recipient_details_form/', views.recipient_details_form, name='recipient_details_form'),
     path('dashboard/recipient/edit/', views.recipient_details_edit, name='recipient_edit'),
-    path('dashboard/recipient/info', views.recipient_dashboard, name='recipient_dashboard'),
+    path('dashboard/recipient/info/', views.recipient_dashboard, name='recipient_dashboard'),
 
 
     path('dashboard/blood_stock/', views.blood_stock_dashboard, name='blood_stock_dashboard'),
     path('dashboard/add_blood/', views.add_blood, name='add_blood'),
     
-    path('forgot_password', views.forgot_password, name='forgot_password'),
+    path('forgot_password/', views.forgot_password, name='forgot_password'),
     path('reset_password/<str:email>/', views.reset_password, name='reset_password'),
 
     path('donor/add/', views.donor_create, name='donor_add'),
